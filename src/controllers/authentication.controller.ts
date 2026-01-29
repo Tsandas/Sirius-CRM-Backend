846e5e24c2b206f141c77bfe62d2f2d3b6377aa6
Tsandas <georgetsandekidis@gmail.com>
2026-01-29 22:29:40 +0200

    coock

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -41,15 +41,17 @@ export const authenticationLogin = async (
     if (client === "web") {
       res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        // secure: process.env.NODE_ENV === "production",
+        secure: false,
+        sameSite: "none",
         maxAge: 15 * 60 * 1000,
       });
 
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        // secure: process.env.NODE_ENV === "production",
+        secure: false,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
